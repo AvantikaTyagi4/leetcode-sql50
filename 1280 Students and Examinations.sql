25622cd271a4ad8b32cd05bbf007b2451c5ddc66
Avantika Tyagi <49015691+AvantikaTyagi4@users.noreply.github.com>
2024-04-27 15:58:47 +0530

    Create 1280 Students and Examinations.sql

---
+++ 1280 Students and Examinations.sql
@@ -0,0 +1,7 @@
+SELECT s.student_id, s.student_name, su.subject_name, count(e.subject_name) attended_exams
+FROM students s CROSS JOIN subjects su
+LEFT JOIN examinations e
+ON s.student_id = e.student_id
+AND e.subject_name = su.subject_name
+GROUP BY s.student_id, s.student_name, su.subject_name
+ORDER BY s.student_id, su.subject_name;
